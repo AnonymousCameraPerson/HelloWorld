4cd8623c554bd9e5f7dc638490eac5e78da0df9e
AnonymousCameraPerson <lucia@luciadonley.com>
2026-05-12 12:55:57 -0400

    Labeled second program in a commend

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,6 +1,6 @@
 // HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+//Second program in Lecture Notes
 #include <stdio.h>
 #include <allegro5/allegro.h>
 int main(int argc, char** argv) {
